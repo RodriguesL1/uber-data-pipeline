8f0efa7da5667d28c6c0a2f14c919b2e280a0396
Leonardo Santos Rodrigues <84885441+RodriguesL1@users.noreply.github.com>
2026-03-25 19:07:13 -0300

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,3 +1,5 @@
-def transform_data(df):
-    df = df.dropna()
-    return df
+from sqlalchemy import create_engine
+
+def load_data(df):
+    engine = create_engine("postgresql://user:password@localhost:5432/uber_db")
+    df.to_sql("corridas", engine, if_exists="replace", index=False)
